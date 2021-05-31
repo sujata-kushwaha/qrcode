5257dae86a4f2a602bc0bf3f7af385555952030e
sujata kushwaha <83424946+sujata-kushwaha@users.noreply.github.com>
2021-05-31 23:35:27 +0530

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,8 @@
+import pyqrcode
+# import png
+import svg
+from pyqrcode import QRCode
+a="i'm anuradha kushwaha now i'm studing btech feild in a IT"
+url=pyqrcode.create(a)
+url.svg("myqr.svg",scale=7)
+# url.png("myqr.png",scale=8)
